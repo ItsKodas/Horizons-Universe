5487a98c06cdff098115d0f2eb147e923f91dc5c
Jess <koda@horizons.gg>
2021-07-22 16:00:17 +1000

    World Template & Basic World Configs

--- Sector/start.bat
+++ Sector/start.bat
@@ -7,10 +7,10 @@ C:/Applications/wget.exe https://build.torchapi.net/job/Torch/job/Torch/job/mast
 powershell Expand-Archive -Force torch-server.zip ./
 del torch-server.zip
 
-powershell move "Torch.Server.exe" "Sol.Server.exe" -force
-powershell move "Torch.Server.exe.config" "Sol.Server.exe.config" -force
-powershell move "Torch.Server.pdb" "Sol.Server.pdb" -force
-powershell move "Torch.Server.xml" "Sol.Server.xml" -force
+powershell move "Torch.Server.exe" "Sector.Server.exe" -force
+powershell move "Torch.Server.exe.config" "Sector.Server.exe.config" -force
+powershell move "Torch.Server.pdb" "Sector.Server.pdb" -force
+powershell move "Torch.Server.xml" "Sector.Server.xml" -force
 
-start Sol.Server.exe -autostart
+start Sector.Server.exe -autostart
 exit
